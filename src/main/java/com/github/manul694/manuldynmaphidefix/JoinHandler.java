4dd725a566ef325b63927789d28a9f47545581cc
Антон <48861382+manul694@users.noreply.github.com>
2023-03-11 17:17:20 +0600

    Uploaded files

---
+++ src/main/java/com/github/manul694/manuldynmaphidefix/JoinHandler.java
@@ -0,0 +1,17 @@
+package com.github.manul694.manuldynmaphidefix;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinHandler implements Listener {
+    @EventHandler
+    public void onJoin (PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        if(!p.hasPermission("dynmap.hide.self")) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "dynmap hide " + p.getName());
+        }
+    }
+}
---
+++ src/main/java/com/github/manul694/manuldynmaphidefix/ManulDynmapHideFix.java
@@ -0,0 +1,18 @@
+package com.github.manul694.manuldynmaphidefix;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ManulDynmapHideFix extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(new JoinHandler(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
